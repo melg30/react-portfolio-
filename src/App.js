d33e9292381eeb386054f64e12d993d1f80d8202
Mel Garza <melgarza3016@gmail.com>
2020-04-16 20:44:26 -0500

    nav and router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
