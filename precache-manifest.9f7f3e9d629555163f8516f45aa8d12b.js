a93adb9ee021b3d38071bd467f85825b9dc6d3ce
Mel Garza <melgarza3016@gmail.com>
2020-04-21 22:55:56 -0500

    Updates

---
+++ precache-manifest.9f7f3e9d629555163f8516f45aa8d12b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de3cdc222d9afc274c8ee3d8fba273e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "cf4b90873294533f8b79",
+    "url": "/react-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a9e00004d2f99aada4ca",
+    "url": "/react-portfolio/static/css/main.cb650f64.chunk.css"
+  },
+  {
+    "revision": "cf4b90873294533f8b79",
+    "url": "/react-portfolio/static/js/2.3a682004.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.3a682004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e00004d2f99aada4ca",
+    "url": "/react-portfolio/static/js/main.44b4dbd1.chunk.js"
+  },
+  {
+    "revision": "2166c6174eb794d71bec",
+    "url": "/react-portfolio/static/js/runtime-main.765787eb.js"
+  },
+  {
+    "revision": "33545ec6ffda90e786dea627ce363688",
+    "url": "/react-portfolio/static/media/memphis-mini.33545ec6.png"
+  }
+]);
